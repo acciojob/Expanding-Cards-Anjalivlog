90149c6ed1811003a687403d632d0d0f1c7cc89b
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-06 13:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function expandCard(panelId) {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+    const panel = document.getElementById(panelId);
+    panel.classList.add('active');
+}
